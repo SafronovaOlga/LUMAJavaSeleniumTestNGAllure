97428ded279cf0e7b6a869b7c0e4c442f72c0158
SafronovaOlga <104982522+SafronovaOlga@users.noreply.github.com>
2024-06-12 16:32:52 +0200

    Updated POM file (#38)
    * Updated POM file
    
    * Created Page Object

---
+++ src/main/java/com/lumatest/model/GearPage.java
@@ -0,0 +1,1 @@
+package com.lumatest.model;import org.openqa.selenium.WebDriver;public class GearPage extends SideMenu {        protected GearPage(WebDriver driver) {        super(driver);    }}
